0caad973445639110602691012a4bcb1be6e3db5
“Garrett <garrett.paul.arnett@gmail.com>
2023-03-27 15:35:20 -0500

    updated

--- wrangle.py
+++ wrangle.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import env
-import sklearn.preprocessing
-from sklearn.model_selection import train_test_split
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # For Zillow Data
