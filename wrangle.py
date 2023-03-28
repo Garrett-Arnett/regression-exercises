ff768dec14e2af86ee6c7af2a085afad3440fce0
“Garrett <garrett.paul.arnett@gmail.com>
2023-03-28 15:51:27 -0500

    updated

--- wrangle.py
+++ wrangle.py
@@ -1,35 +1,171 @@
+
+'''Wrangles data from Zillow Database'''
+
+##################################################Wrangle.py###################################################
+
 import pandas as pd
-import numpy as np
-import env
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# For Zillow Data
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def get_connection(db, user=env.user, host=env.host, password=env.password):
-    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+from env import user, password, host
+
+#**************************************************Acquire*******************************************************
 
 def acquire_zillow():
-    '''
-    Grab our data from path and read as csv
-    '''
+    ''' Acquire data from Zillow using env imports and rename columns'''
     
-    df = pd.read_sql('SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips FROM properties_2017 join predictions_2017 using(parcelid) where propertylandusetypeid = 261;', get_connection('zillow'))
+    url = f"mysql+pymysql://{user}:{password}@{host}/zillow"
+    
+    query = """
+            
+    SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+    FROM properties_2017
 
-    return(df)
-    
-def clean_zillow(df):
-    '''
-    Takes in a df of zillow_data and cleans the data 
-    appropriatly by dropping nulls.
+    LEFT JOIN propertylandusetype USING(propertylandusetypeid)
 
-    return: df, a cleaned pandas data frame.
-    '''
+    WHERE propertylandusedesc IN ("Single Family Residential",                       
+                                  "Inferred Single Family Residential")"""
+
+    # get dataframe of data
+    df = pd.read_sql(query, url)
     
-    # Instead of using dummies to seperate contracts use, 
-    # df[['Contract']].value_counts()
-    # Use a SQL querry
     
-    df = df
-    df = df.dropna()
+    # renaming column names to one's I like better
+    df = df.rename(columns = {'bedroomcnt':'bedrooms', 
+                              'bathroomcnt':'bathrooms', 
+                              'calculatedfinishedsquarefeet':'area',
+                              'taxvaluedollarcnt':'tax_value', 
+                              'yearbuilt':'year_built',})
     return df
+
+#**************************************************Remove Outliers*******************************************************
+
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
     
+    for col in col_list:
+
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
+        
+    return df
+
+#**************************************************Distributions*******************************************************
+
+def get_hist(df):
+    ''' Gets histographs of acquired continuous variables'''
+    
+    plt.figure(figsize=(16, 3))
+
+    # List of columns
+    cols = [col for col in df.columns if col not in ['fips', 'year_built']]
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot nos should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display histogram for column.
+        df[col].hist(bins=5)
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # turn off scientific notation
+        plt.ticklabel_format(useOffset=False)
+
+        plt.tight_layout()
+
+    plt.show()
+        
+        
+def get_box(df):
+    ''' Gets boxplots of acquired continuous variables'''
+    
+    # List of columns
+    cols = ['bedrooms', 'bathrooms', 'area', 'tax_value', 'taxamount']
+
+    plt.figure(figsize=(16, 3))
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display boxplot for column.
+        sns.boxplot(data=df[[col]])
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # sets proper spacing between plots
+        plt.tight_layout()
+
+    plt.show()
+        
+#**************************************************Prepare*******************************************************
+
+def prepare_zillow(df):
+    ''' Prepare zillow data for exploration'''
+
+    # removing outliers
+    df = remove_outliers(df, 1.5, ['bedrooms', 'bathrooms', 'area', 'tax_value', 'taxamount'])
+    
+    # get distributions of numeric data
+    get_hist(df)
+    get_box(df)
+    
+    # converting column datatypes
+    df.fips = df.fips.astype(object)
+    df.year_built = df.year_built.astype(object)
+    
+    # train/validate/test split
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    # impute year built using mode
+    imputer = SimpleImputer(strategy='median')
+
+    imputer.fit(train[['year_built']])
+
+    train[['year_built']] = imputer.transform(train[['year_built']])
+    validate[['year_built']] = imputer.transform(validate[['year_built']])
+    test[['year_built']] = imputer.transform(test[['year_built']])       
+    
+    return train, validate, test    
+
+
+#**************************************************Wrangle*******************************************************
+
+
+def wrangle_zillow():
+    '''Acquire and prepare data from Zillow database for explore'''
+    train, validate, test = prepare_zillow(acquire_zillow())
+    
+    return train, validate, test
